b8eca905105411becab677efdbf0019c21bae4d9
Iskander Sharipov <quasilyte@gmail.com>
2023-11-01 12:23:05 +0400

    fix a couple of comment formatting issue
    This is a followup for #33

--- internal_min.go
+++ internal_min.go
@@ -2,7 +2,7 @@ package input
 
 import "golang.org/x/exp/constraints"
 
-// minOf is a placeholder implementation until Go 1.21's builtin min implementation is available
+// minOf is a placeholder implementation until Go 1.21's builtin min implementation is available.
 func minOf[T constraints.Ordered](a, b T) T {
 	if a < b {
 		return a
